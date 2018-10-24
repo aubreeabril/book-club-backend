3c8d29ed05440b9bf7045c8bbd67091792f4e82d
aubreeabril <aubree.abril@gmail.com>
2018-10-24 14:17:23 -0400

    fixing db

--- db/migrate/20181012143946_remove_password_digest_from_users.rb
+++ db/migrate/20181012143946_remove_password_digest_from_users.rb
@@ -1,6 +1,6 @@
 class RemovePasswordDigestFromUsers < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :passwordDigest, :string
+    remove_column :users, :password_digest, :string
     add_column :users, :picture, :string
     add_column :users, :auth0sub, :string
   end
